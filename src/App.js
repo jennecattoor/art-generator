94984f0a93d43fe4b7d0a8e8700b3fa3cf3d7ebe
Jenneeee <jenne@cattoor.net>
2022-02-22 16:03:55 +0100

    Working further on the assignment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import Wolf from './icons/Wolf.jsx';
+import Slider from './components/Slider'
+import { useState } from 'react';
 
 function App() {
+    const [amount, setAmount] = useState(25);
   return (
-    <>
-    <p>Hi mom</p>
-    </>
+    <div className="container">
+      <section className='info-input'>
+        <div className="text">
+          <div className='title-wrapper'>
+            <h1>The forest by night</h1><div className="icon-wolf"><Wolf /></div>
+          </div>
+          <p>Walking alone in a forest in the middle of the night can be scary. Luckily you can experience it online instead of putting yourself at risk</p>
+        </div>
+        <div className="input">
+        <Slider max={50} label="Amount of wolves:" value={amount} onValueChange={v => setAmount(v)}/>
+        </div>
+      </section>
+    </div>
   );
 }
 
---
+++ src/components/Slider.js
@@ -0,0 +1,30 @@
+import PropTypes from "prop-types";
+
+const Slider = ({ onValueChange, value, max, min, label }) => {
+  return (
+    <label>
+      <p>{label} <span className="text-bold">{value}</span></p>
+      <input
+        type="range"
+        min={min}
+        max={max}
+        value={value}
+        onChange={(e) => onValueChange(parseInt(e.target.value, 10))}
+      />
+    </label>
+  );
+};
+
+Slider.defaultProps = {
+  min: 0
+}
+
+Slider.propTypes = {
+  onValueChange: PropTypes.func.isRequired,
+  value: PropTypes.number.isRequired,
+  max: PropTypes.number.isRequired,
+  min: PropTypes.number.isRequired,
+  label: PropTypes.string.isRequired,
+};
+
+export default Slider;
